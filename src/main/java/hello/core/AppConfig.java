38be61f1acca5453bc28a5d5db874c510ee7e739
ssc <tmdcjf991203@gmail.com>
2023-06-24 00:04:30 +0900

    입ë spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,25 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //설정정보 , 이후에 밑에 코드들에 @Bean을 붙히면 전부 스프링 컨테이너에 등록됨
 public class AppConfig {
-
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
-    private static DiscountPolicy discountPolicy() {
+    @Bean
+    public static DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
 //        return new RateDiscountPolicy();
     }
